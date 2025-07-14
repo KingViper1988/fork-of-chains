1899cc08c7ebf77f18f05b0d8ad21c17cd2242d0
KingViper1988 <longkiller1988@gmail.com>
2025-07-14 20:49:43 +0700

    Update building.ts

--- src/scripts/text/macro/building.ts
+++ src/scripts/text/macro/building.ts
@@ -1,10 +1,9 @@
 // @ts-nocheck
 
 /*
-  <<inyourbuilding 'bazaar'>>: "in your bazaar", or "once you build a bazaar"
+  <<inyourbuilding 'bazaar'>>: "trong khu chợ của bạn", hoặc "một khi bạn xây một khu chợ"
 */
 
 Macro.add('inyourbuilding', { handler() {
   this.output.append(setup.DOM.Util.twine(setup.Text.Building.inYourBuilding(this.args[0])))
 } });
-
